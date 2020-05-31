b8de4d7f3fb8bdd2ccf0aaa0af33e94b7a0c88a7
mun-sathish <mun.sathish@gmail.com>
2020-05-31 19:39:30 +0530

    Init push

---
+++ precache-manifest.baa2ab3cef9a951a74803d2834a57fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5c34ba66209fd65e02d0bc6258140b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7990fa1f4f52dd8a77c8",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "7bab45ace1e2c5448ce1",
+    "url": "/static/css/main.e2bcc207.chunk.css"
+  },
+  {
+    "revision": "7990fa1f4f52dd8a77c8",
+    "url": "/static/js/2.51b9e568.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.51b9e568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bab45ace1e2c5448ce1",
+    "url": "/static/js/main.c42bbb9b.chunk.js"
+  },
+  {
+    "revision": "aa69d55ccb36765122d6",
+    "url": "/static/js/runtime-main.fecb262b.js"
+  }
+]);
